62d3e3b79f29109ae6875d75686c3fcb66c471ce
Equiphract <nazgul@bluewin.ch>
2022-08-20 00:53:39 +0200

    Implements simple notification service

---
+++ src/main/java/me/equiphract/markdownviewer/model/filereader/FileContentChangeNotifier.java
@@ -0,0 +1,28 @@
+package me.equiphract.markdownviewer.model.filereader;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Consumer;
+
+public final class FileContentChangeNotifier {
+
+  private Map<Object, Consumer<String>> subscribers = new HashMap<>();
+
+  public void subscribe(Object subscriber, Consumer<String> callback) {
+    if (subscriber == null || callback == null) {
+      throw new IllegalArgumentException("Null values are not permitted.");
+    }
+
+    subscribers.put(subscriber, callback);
+  }
+
+  public void publish(String changedFileContent) {
+    subscribers.values().forEach(s -> s.accept(changedFileContent));
+  }
+
+  public void unsubscribe(Object subscriber) {
+    subscribers.remove(subscriber);
+  }
+
+}
+
---
+++ src/test/java/me/equiphract/markdownviewer/model/filereader/FileContentChangeNotifierTest.java
@@ -0,0 +1,60 @@
+package me.equiphract.markdownviewer.model.filereader;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.only;
+import static org.mockito.Mockito.verify;
+
+import java.util.function.Consumer;
+
+import org.junit.jupiter.api.Test;
+
+class FileContentChangeNotifierTest {
+
+  FileContentChangeNotifier listener = new FileContentChangeNotifier();
+
+  @Test
+  @SuppressWarnings("unchecked")
+  void subscribe_givenSubscriber_notifiesSubscriber() {
+    Object subscriber = new Object();
+    Consumer<String> consumer = mock(Consumer.class);
+
+    listener.subscribe(subscriber, consumer);
+    listener.publish("");
+
+    verify(consumer, only()).accept("");
+  }
+
+  @Test
+  @SuppressWarnings("unchecked")
+  void subscribe_givenNullSubscriber_throwsIllegalArgumentException() {
+    assertThrows(IllegalArgumentException.class,
+                 () -> listener.subscribe(null, mock(Consumer.class)));
+  }
+
+  @Test
+  void subscribe_givenNullCallback_throwsIllegalArgumentException() {
+    assertThrows(IllegalArgumentException.class,
+                 () -> listener.subscribe(new Object(), null));
+  }
+
+  @Test
+  @SuppressWarnings("unchecked")
+  void unsubscribe_givenSubscriber_subscriberDoesntGetNotifiedAnymore() {
+    Object subscriber = new Object();
+    Consumer<String> consumer = mock(Consumer.class);
+
+    listener.subscribe(subscriber, consumer);
+    listener.publish("1");
+
+    verify(consumer, only()).accept("1");
+
+    listener.unsubscribe(subscriber);
+    listener.publish("2");
+
+    verify(consumer, never()).accept("2");
+  }
+
+}
+
