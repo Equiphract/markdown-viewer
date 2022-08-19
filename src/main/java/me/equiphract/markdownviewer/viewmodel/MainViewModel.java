0783fff4a95d493b873eb4d4ac787b329bf6a97f
Equiphract <nazgul@bluewin.ch>
2022-08-20 01:08:30 +0200

    Implements the main view's ViewModel

---
+++ src/main/java/me/equiphract/markdownviewer/viewmodel/MainViewModel.java
@@ -0,0 +1,51 @@
+package me.equiphract.markdownviewer.viewmodel;
+
+import java.io.IOException;
+import java.nio.file.Path;
+
+import javafx.application.Platform;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.beans.value.ChangeListener;
+import me.equiphract.markdownviewer.model.filereader.FileObserver;
+import me.equiphract.markdownviewer.model.markdown.MarkdownConverter;
+import me.equiphract.markdownviewer.model.markdown.MarkdownToHtmlConverter;
+
+public final class MainViewModel {
+
+  private StringProperty pageHtml;
+  private FileObserver fileObserver;
+  private MarkdownConverter converter;
+
+  public MainViewModel() throws IOException, InterruptedException {
+    pageHtml = new SimpleStringProperty("");
+    fileObserver = new FileObserver();
+    converter = new MarkdownToHtmlConverter();
+
+    fileObserver.subscribeToChangeNotifier(this, this::updatePageHtmlProperty);
+    Path file = Path.of("/tmp/markdown.md");
+    fileObserver.observe(file);
+  }
+
+  private void updatePageHtmlProperty(String modifiedFileContent) {
+    String convertedFileContent = converter.convert(modifiedFileContent);
+    pageHtml.set(convertedFileContent);
+  }
+
+  public void addAsyncPageHtmlPropertyListener(
+      ChangeListener<? super String> listener) {
+
+    wrapListenerInRunLater(listener);
+    pageHtml.addListener(wrapListenerInRunLater(listener));
+  }
+
+  private ChangeListener<? super String> wrapListenerInRunLater(
+      ChangeListener<? super String> listener) {
+
+    return (observable, oldValue, newValue) -> {
+      Platform.runLater(() -> listener.changed(observable, oldValue, newValue));
+    };
+  }
+
+}
+
