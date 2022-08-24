afb090fa34a30d20300a60d2a76e0dbfc4424ed9
Equiphract <nazgul@bluewin.ch>
2022-08-24 16:34:43 +0200

    Implements first test for main view

---
+++ src/test/java/me/equiphract/markdownviewer/view/MainViewTest.java
@@ -0,0 +1,42 @@
+package me.equiphract.markdownviewer.view;
+
+import static org.testfx.api.FxAssert.verifyThat;
+import static org.testfx.matcher.base.NodeMatchers.isVisible;
+
+import java.io.IOException;
+import java.net.URL;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.testfx.framework.junit5.ApplicationExtension;
+import org.testfx.framework.junit5.Start;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+@ExtendWith(ApplicationExtension.class)
+class MainViewTest {
+
+  @Start
+  private void start(Stage stage) throws IOException {
+    var fxmlLoader = new FXMLLoader(getMainFxmlLocationUrl());
+    var sceneRootObject = (Parent) fxmlLoader.load();
+    var mainScene = new Scene(sceneRootObject, 300, 600);
+    stage.setScene(mainScene);
+    stage.show();
+  }
+
+  private URL getMainFxmlLocationUrl() {
+    String mainFxmlLocation = "/me/equiphract/markdownviewer/view/MainView.fxml";
+    return getClass().getResource(mainFxmlLocation);
+  }
+
+  @Test
+  void webView_shouldExist() {
+    verifyThat("#webView", isVisible());
+  }
+
+}
+
