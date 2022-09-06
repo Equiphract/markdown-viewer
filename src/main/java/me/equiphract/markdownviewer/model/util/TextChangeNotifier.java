bc92bfdbeaf7233be9c9a06a99f77d798550ad07
Equiphract <nazgul@bluewin.ch>
2022-09-07 00:24:31 +0200

    Adds missing `@Override` annotations

--- src/main/java/me/equiphract/markdownviewer/model/util/TextChangeNotifier.java
+++ src/main/java/me/equiphract/markdownviewer/model/util/TextChangeNotifier.java
@@ -8,6 +8,7 @@ public final class TextChangeNotifier implements ChangeNotifier<String> {
 
   private Map<Object, Consumer<String>> subscribers = new HashMap<>();
 
+  @Override
   public void subscribe(Object subscriber, Consumer<String> callback) {
     if (subscriber == null || callback == null) {
       throw new IllegalArgumentException("Null values are not permitted.");
@@ -16,10 +17,12 @@ public final class TextChangeNotifier implements ChangeNotifier<String> {
     subscribers.put(subscriber, callback);
   }
 
+  @Override
   public void unsubscribe(Object subscriber) {
     subscribers.remove(subscriber);
   }
 
+  @Override
   public void publish(String changedFileContent) {
     subscribers.values().forEach(s -> s.accept(changedFileContent));
   }
