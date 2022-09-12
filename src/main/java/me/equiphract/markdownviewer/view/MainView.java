16661270e121240c7004f2ea60df42d88012c3cd
Equiphract <nazgul@bluewin.ch>
2022-09-12 03:39:45 +0200

    Implements scroll position persistence

--- src/main/java/me/equiphract/markdownviewer/view/MainView.java
+++ src/main/java/me/equiphract/markdownviewer/view/MainView.java
@@ -4,9 +4,12 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
+import javafx.beans.property.ReadOnlyProperty;
 import javafx.beans.value.ObservableValue;
+import javafx.concurrent.Worker.State;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 import javafx.stage.FileChooser;
 import javafx.stage.Window;
@@ -16,9 +19,12 @@ public final class MainView {
 
   private MainViewModel mainViewModel;
   private FileChooser fileChooser;
+  private int horizontalScrollPosition;
+  private int verticalScrollPosition;
 
   @FXML
   private WebView webView;
+  private WebEngine webEngine;
 
   public MainView() throws IOException, InterruptedException {
     mainViewModel = new MainViewModel();
@@ -26,7 +32,10 @@ public final class MainView {
 
   @FXML
   protected void initialize() {
+    webEngine = webView.getEngine();
+
     addHtmlPropertyListener();
+    addPageLoadingStatePropertyListener();
   }
 
   private void addHtmlPropertyListener() {
@@ -38,7 +47,32 @@ public final class MainView {
       String oldValue,
       String newValue) {
 
-    webView.getEngine().loadContent(newValue);
+    webEngine.loadContent(newValue);
+  }
+
+  private void addPageLoadingStatePropertyListener() {
+    ReadOnlyProperty<State> pageLoadingStateProperty =
+      webEngine.getLoadWorker().stateProperty();
+
+    pageLoadingStateProperty
+      .addListener(this::scrollToPreviousPositionIfPageLoaded);
+  }
+
+  private void scrollToPreviousPositionIfPageLoaded(
+      ObservableValue<? extends State> stateProperty,
+      State oldValue,
+      State newValue) {
+
+    if (newValue == State.SUCCEEDED) {
+      scrollToLastKnownPosition();
+    }
+  }
+
+  private void scrollToLastKnownPosition() {
+    String jsScrollTo = "window.scrollTo(%s, %s)"
+      .formatted(verticalScrollPosition, horizontalScrollPosition);
+
+    webEngine.executeScript(jsScrollTo);
   }
 
   @FXML
@@ -52,12 +86,26 @@ public final class MainView {
     Window mainWindow = webView.getScene().getWindow();
     File selectedFile = fileChooser.showOpenDialog(mainWindow);
 
+    resetScrollPosition();
     mainViewModel.loadFile(selectedFile);
   }
 
   private void createFileChooser() {
     fileChooser = new FileChooser();
     fileChooser.setTitle("Open Markdown File");
+  }
+
+  private void resetScrollPosition() {
+    horizontalScrollPosition = 0;
+    verticalScrollPosition = 0;
+  }
+
+  @FXML
+  private void rememberScrollPosition() {
+    horizontalScrollPosition =
+      (Integer) webEngine.executeScript("document.body.scrollTop");
+    verticalScrollPosition =
+      (Integer) webEngine.executeScript("document.body.scrollLeft");
   }
 
 }
